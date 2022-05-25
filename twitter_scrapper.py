91f1b532bf9e726f3ff2c17a8976cb3448393994
SOnne101 <31986858+SOnne101@users.noreply.github.com>
2022-05-25 20:35:23 +0200

    Add files via upload

---
+++ twitter_scrapper.py
@@ -0,0 +1,35 @@
+#Imports
+import tweepy
+import configparser
+import pandas as pd
+
+#Read configs
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+api_key = config['twitter']['API_Key']
+api_key_secret = config['twitter']['API_Key_Secret']
+
+access_token = config['twitter']['Access_Token']
+access_token_secret = config['twitter']['Access_Token_Secret']
+
+#Authentication
+auth = tweepy.OAuthHandler(api_key, api_key_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+#Policitians id
+ids = ['Statsmin','JakobEllemann','PiaOlsen', 'sofiecn', 'MaiVilladsen', 'SorenPape', 'MrMesserschmidt', 
+        'PernilleVermund', 'AlexVanopslagh', 'SikandaSIDDIQUE', 'FranciskaRosenk', 'IsabellaArendt']
+
+#Get tweet
+
+tweets = []
+
+for id in range(len(ids)):
+    for i in tweepy.Cursor(api.user_timeline, id=ids[id], tweet_mode='extended').items(100):
+        tweets.append(i.full_text)
+
+df = pd.DataFrame({'Tweets':tweets})
+df.to_csv('tweet.csv', index=False)
